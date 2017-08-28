3a0cc089dd9c3c69891c7f86483f23b2ba355f82
kaustubhkv <kaustubh.itkurkar@gmail.com>
2017-08-28 11:38:41 -0700

    Basics

--- index.android.js
+++ index.android.js
@@ -1,62 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import {Text, View, AppRegistry} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import ToDoApp from './ToDoApp/ToDoAppComponent';
+import Component4 from './app/components/Component4/Component4';
 
 export default class MyApp extends Component {
-
-   render () {
-    return (
-
-      <ToDoApp />
-  );
-  }
-  // render() {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text style={styles.welcome}>
-  //         My first React App!
-  //       </Text>
-  //       <Text style={styles.instructions}>
-  //         To get started, edit index.android.js
-  //       </Text>
-  //       <Text style={styles.instructions}>
-  //         Double tap R on your keyboard to reload,{'\n'}
-  //         Shake or press menu button for dev menu
-  //       </Text>
-  //     </View>
-  //   );
-  // }
+	render() {
+		return (
+			<View>
+				<Component4 />
+			</View>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('MyApp', () => MyApp);
